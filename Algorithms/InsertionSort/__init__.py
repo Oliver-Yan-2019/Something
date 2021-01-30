eaaaed98839b3a094b3973feab57de13c23b912d
严梓桓 <1831929935@qq.com>
2021-01-30 21:31:18 +0800

    improvement(排序算法): 修改插入排序的相关描述

--- Algorithms/InsertionSort/__init__.py
+++ Algorithms/InsertionSort/__init__.py
@@ -7,7 +7,7 @@ def insertion_sort(arr):
     """
     插入排序 O(n) - 双指针实现方式
 
-    将大于待排元素的往后挪
+    将元素从未排序序列挪至已排序序列
     :param arr:
     :return:
     """
@@ -16,10 +16,10 @@ def insertion_sort(arr):
         cur = arr[i]  # 当前待排元素
         j = i
         while j > 0 and arr[j - 1] > cur:
-            arr[j] = arr[j - 1]  # 将大于待排元素的元素往后挪
+            arr[j] = arr[j - 1]  # 将大于待排元素的元素往后挪(未排序序列)
             j -= 1
 
-        arr[j] = cur
+        arr[j] = cur  # 将待排元素放到已排序序列
 
 
 if __name__ == '__main__':
